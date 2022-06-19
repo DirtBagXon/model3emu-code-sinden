e4f366c41600ed40634120887868ceaaedeae266
DirtBagXon <noop@member.fsf.org>
2022-06-19 22:10:38 +0100

    0.3a-r887a

--- Src/Version.h
+++ Src/Version.h
@@ -29,7 +29,7 @@
 #define INCLUDED_VERSION_H
 
 #ifndef SUPERMODEL_VERSION
-#define SUPERMODEL_VERSION	"0.3a-r886a DirtBagXon (Sinden)"
+#define SUPERMODEL_VERSION	"0.3a-r887a DirtBagXon (Sinden)"
 #endif
 
 #endif	// INCLUDED_VERSION_H
