ac19d512ee56cb5ee0917c6e596f9df75d97a5dc
DirtBagXon <noop@member.fsf.org>
2025-04-01 00:16:58 +0100

    0.3a-12e3b6a

--- Src/Version.h
+++ Src/Version.h
@@ -29,7 +29,7 @@
 #define INCLUDED_VERSION_H
 
 #ifndef SUPERMODEL_VERSION
-#define SUPERMODEL_VERSION	"0.3a-90b5bfc-mm DirtBagXon (Sinden)"
+#define SUPERMODEL_VERSION	"0.3a-12e3b6a-mm DirtBagXon (Sinden)"
 #endif
 
 #endif	// INCLUDED_VERSION_H
