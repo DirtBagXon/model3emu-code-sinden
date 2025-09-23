546acee956ddbf3920b4384f016c2d143fac959a
DirtBagXon <noop@member.fsf.org>
2025-09-23 19:33:42 +0100

    0.3a-4e5905f

--- Src/Inputs/Input.h
+++ Src/Inputs/Input.h
@@ -34,6 +34,7 @@
 #include "Types.h"
 #include "Game.h"
 #include "Util/NewConfig.h"
+#include <memory>
 
 class CInputSystem;
 
@@ -75,7 +76,7 @@ private:
 	const char *m_defaultMapping;
 	
 	// Assigned input system
-	CInputSystem *m_system;
+	std::shared_ptr<CInputSystem> m_system;
 
 	/*
 	 * Creates an input source using the current input system and assigns it to this input.
@@ -116,7 +117,12 @@ public:
 	/*
 	 * Initializes this input with the given input system.  Must be called before any other methods are used.
 	 */
-	void Initialize(CInputSystem *system);
+	void Initialize(std::shared_ptr<CInputSystem> system);
+
+	/*
+    * Initializes this input with the given input system.  Must be called before any other methods are used.
+    */
+	std::shared_ptr<CInputSystem> GetInputSystem();
 
 	/*
 	 * Returns the name of the group of controls that this input belongs to.
